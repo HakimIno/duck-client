d0bd839a7cae66b768c70486e15dd9b09d2c0d32
weerachit utnok <weerachit.utnok01@gmail.com>
2024-01-08 14:29:49 +0700

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  server: {
+    port: 3030,
+  },
+  preview: {
+    port: 8080,
+  },
 })
